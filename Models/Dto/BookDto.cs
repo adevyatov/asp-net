425b39afa3b1bc73beab009ffef6f5490cc1f6d8
Alexey Devyatov <alexey@devyatov.net>
2021-11-05 00:44:14 +0300

    1qkveww: 1.2.2 Add Book dto

---
+++ Models/Dto/BookDto.cs
@@ -0,0 +1,14 @@
+namespace WebApi.Models.Dto
+{
+    /// <summary>
+    ///     1.2.2 - Класс книги
+    /// </summary>
+    public class BookDto
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+    }
+}

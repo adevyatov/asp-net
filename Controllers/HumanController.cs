876864277805f1f6e342804acf517fbc4194884d
Alexey Devyatov <alexey@devyatov.net>
2021-11-05 15:16:03 +0300

    1qkveww: 1.3.2. Add method that returns writers

--- Controllers/HumanController.cs
+++ Controllers/HumanController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Db;
 using WebApi.Models.Dto;
@@ -15,9 +16,23 @@ namespace WebApi.Controllers
         /// 1.3.1 - Список всех людей
         /// </summary>
         [HttpGet("humans")]
-        public IEnumerable<HumanDto> Humans()
+        public IEnumerable<HumanDto> GetHumans()
         {
             return Database.Humans.ToArray();
+        }
+
+        /// <summary>
+        /// 1.3.2 - Список людей, являющихся писателями
+        /// </summary>
+        [HttpGet("humans/writers")]
+        public IEnumerable<HumanDto> GetWriters()
+        {
+            return
+                (
+                    from h in Database.Humans
+                    join b in Database.Books on h.Id equals b.Author
+                    select h
+                ).Distinct();
         }
     }
 }

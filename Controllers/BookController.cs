266de12ddf8f8bc2b06cf0532aa3ae9758eaa269
Alexey Devyatov <alexey@devyatov.net>
2021-11-05 23:58:55 +0300

    1qkveww: 1.4.2 - 1.4.3 Add POST and DELETE methods that allows add and delete books

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Db;
 using WebApi.Models.Dto;
+using WebApi.ViewModels;
 
 namespace WebApi.Controllers
 {
@@ -10,7 +11,7 @@ namespace WebApi.Controllers
     public class BookController : Controller
     {
         /// <summary>
-        ///     1.4.1 - Список книг
+        ///     1.4.1.1 - Список книг
         /// </summary>
         [HttpGet]
         public IEnumerable<BookDto> GetBooks()
@@ -19,12 +20,48 @@ namespace WebApi.Controllers
         }
 
         /// <summary>
-        ///     1.4.2 - Добавление новой книги
+        ///     1.4.1.2 - Добавление новой книги
         /// </summary>
         [HttpGet("{authorId}")]
         public IEnumerable<BookDto> GetBooks([FromRoute] int authorId)
         {
             return Database.Books.Where(b => b.Author == authorId).ToArray();
+        }
+
+        /// <summary>
+        ///     1.4.2 - Добавление новой книги
+        /// </summary>
+        [HttpPost]
+        public BookDto AddBook(AddBookViewModel model)
+        {
+            var id = Database.Books.Count + 1;
+
+            var book = new BookDto
+            {
+                Id = id,
+                Title = model.Title,
+                Author = model.Author,
+                Genre = model.Genre,
+            };
+
+            Database.Books.Add(book);
+
+            return book;
+        }
+
+        /// <summary>
+        ///     1.4.3 - Удаление книги
+        /// </summary>
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook([FromRoute] int id)
+        {
+            var book = Database.Books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+
+            // remove book
+            Database.Books.Remove(book);
+
+            return Ok();
         }
     }
 }
---
+++ ViewModels/AddBookViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WebApi.ViewModels
+{
+    public class AddBookViewModel
+    {
+        public string Title { get; set; } = null!;
+
+        public int Author { get; set; }
+
+        public string Genre { get; set; } = null!;
+    }
+}

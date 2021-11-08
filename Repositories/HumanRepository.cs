96b7be8169411d6331c1324d0b42123a3f453699
Alexey Devyatov <alexey@devyatov.net>
2021-11-08 19:56:42 +0300

    1qkveww: Add Human repository

---
+++ Repositories/HumanRepository.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Db;
+using WebApi.Models.Dto;
+
+namespace WebApi.Repositories
+{
+    public class HumanRepository : IHumanRepository
+    {
+        private static List<HumanDto> Humans { get; } = new()
+        {
+            new HumanDto
+            {
+                Id = 1,
+                Surname = "Максвелл Демпси",
+                Name = "Генри",
+                Birthday = DateTime.Parse("1925-03-12"),
+            },
+            new HumanDto
+            {
+                Id = 2,
+                Surname = "Перумов",
+                Name = "Николай",
+                Patronymic = "Даниилович",
+                Birthday = DateTime.Parse("1963-11-21"),
+            },
+            new HumanDto
+            {
+                Id = 3,
+                Surname = "Лукьяненко",
+                Name = "Сергей",
+                Patronymic = "Васильевич",
+                Birthday = DateTime.Parse("1968-04-11"),
+            },
+            new HumanDto
+            {
+                Id = 4,
+                Surname = "Иванов",
+                Name = "Иван",
+                Patronymic = "Иванович",
+                Birthday = DateTime.Parse("1975-07-30"),
+            },
+        };
+
+        private static int LastId { get; set; } = Humans.Count;
+
+        public HumanDto? GetById(int id)
+        {
+            return Humans.FirstOrDefault(h => h.Id == id);
+        }
+
+        public IEnumerable<HumanDto> GetAll()
+        {
+            return Humans.ToArray();
+        }
+
+        public IEnumerable<HumanDto> GetByQuery(string query)
+        {
+            return Humans.FindAll(h =>
+                h.Name.Contains(query) ||
+                h.Surname.Contains(query) ||
+                (h.Patronymic ?? "").Contains(query)
+            ).ToArray();
+        }
+
+        public IEnumerable<HumanDto> GetWriters()
+        {
+            return
+            (
+                from h in Humans
+                join b in Database.Books on h.Id equals b.AuthorId
+                select h
+            ).Distinct();
+        }
+
+        public HumanDto Add(HumanDto human)
+        {
+            human.Id = ++LastId;
+            Humans.Add(human);
+
+            return human;
+        }
+
+        public void Remove(HumanDto human)
+        {
+            Humans.Remove(human);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using WebApi.Repositories;
 
 namespace WebApi
 {
@@ -21,6 +22,7 @@ namespace WebApi
         {
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebApi", Version = "v1"}); });
+            services.AddTransient<IHumanRepository, HumanRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

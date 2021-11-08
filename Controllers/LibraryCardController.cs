56362ccd24a234252c6314ae4aabcd0ccda1d709
Alexey Devyatov <alexey@devyatov.net>
2021-11-08 23:37:58 +0300

    1qkveww: 2.1.2 - LibraryCard controller

---
+++ Controllers/LibraryCardController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    /// <summary>
+    ///     2.1.2 - Контроллер, отвечающий за получение книги человеком
+    /// </summary>
+    public class LibraryCardController : Controller
+    {
+
+    }
+}

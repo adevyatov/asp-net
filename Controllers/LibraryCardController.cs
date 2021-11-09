cd80bc5af2d0b75e83beb05872a2d5a374ba7371
Alexey Devyatov <alexey@devyatov.net>
2021-11-09 20:35:06 +0300

    1qkveww: 2.1.4 - Add POST method in LibraryCard controller that allows to take book by human

--- Controllers/LibraryCardController.cs
+++ Controllers/LibraryCardController.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services;
 
 namespace WebApi.Controllers
 {
     /// <summary>
     ///     2.1.2 - Контроллер, отвечающий за получение книги человеком
     /// </summary>
+    [ApiController]
+    [Route("library-card")]
     public class LibraryCardController : Controller
     {
+        private readonly ILibraryCardService _cardService;
 
+        public LibraryCardController(ILibraryCardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+        /// <summary>
+        ///     2.1.4 - Метод POST отвечающий за взятие книги читателем
+        /// </summary>
+        [HttpPost]
+        public LibraryCard TakeBook([FromQuery] int humanId, [FromQuery] int bookId)
+        {
+            return _cardService.TakeBook(humanId, bookId);
+        }
     }
 }

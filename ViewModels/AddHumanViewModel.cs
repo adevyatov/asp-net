2185b788cb11e74b85e0dcb96cb49bff390ad6f8
Alexey Devyatov <alexey@devyatov.net>
2021-11-09 21:02:58 +0300

    1qkveww: 2.2.1 - Add NotNull validation

--- ViewModels/AddHumanViewModel.cs
+++ ViewModels/AddHumanViewModel.cs
@@ -1,15 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi.ViewModels
 {
     public class AddHumanViewModel
     {
+        [Required]
         public string Name { get; set; } = null!;
 
+        [Required]
         public string Surname { get; set; } = null!;
 
         public string? Patronymic { get; set; }
 
+        [Required]
         public DateTime Birthday { get; set; }
     }
 }

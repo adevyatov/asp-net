48446cc2dda5aec02adf0cf7091c5b72968da799
Alexey Devyatov <alexey@devyatov.net>
2021-11-09 20:37:48 +0300

    1qkveww: 2.1.4 - Add NotFoundException, use custom exception handler

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using WebApi.Exceptions;
 using WebApi.Repositories;
 using WebApi.Services;
 
@@ -40,6 +44,20 @@ namespace WebApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features.Get<IExceptionHandlerPathFeature>().Error;
+                var response = new {error = exception.Message};
+
+                if (exception is HttpStatusException httpStatusException)
+                {
+                    context.Response.StatusCode = (int) httpStatusException.Status;
+                }
+
+                await context.Response.WriteAsJsonAsync(response);
+                await context.Response.CompleteAsync();
+            }));
 
             app.UseHttpsRedirection();
 
